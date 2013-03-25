56e149734860d67951482f4010bcb16ad9de83c9
mherold <herold@emjot.de>
2013-03-25 14:13:42 +0100

    Require rails and add error message for Mongoid/MongoMapper

--- lib/globalize3-jquery-autocomplete.rb
+++ lib/globalize3-jquery-autocomplete.rb
@@ -1,12 +1,15 @@
 require "globalize3_jquery_autocomplete/version"
 require "globalize3_jquery_autocomplete/orm/active_record"
 
+require "rails/all"
 require "globalize3"
 require "rails3-jquery-autocomplete"
 
 ActiveSupport.on_load :action_controller do
   if defined?(Mongoid::Document) || defined?(MongoMapper::Document)
-    # TODO support mongo
+    if Rails.logger.try(:error?)
+      Rails.logger.error("globalize3-jquery-autocomplete only supports ActiveRecord (not Mongoid / MongoMapper)")
+    end
   else
     ActionController::Base.send(:include, Globalize3JQueryAutocomplete::Orm::ActiveRecord)
   end
